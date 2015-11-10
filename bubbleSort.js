cb2c28a4b830a732ef47a88b7dfc8bee7fe038e8
esayli <esayli7@gmail.com>
2015-11-10 10:25:57 -0600

    add bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,23 @@
+var arr  = [45,44,34,16,9,12,3334,234,1765,3452123,1452];
+
+function bubbleShort(unsortedArray){
+    var flag = 0;
+    for(var i = 0 ; i <unsortedArray.length; i++){
+        
+        if(unsortedArray[i]>unsortedArray[i+1]){
+            var temp = unsortedArray[i];
+            unsortedArray[i] = unsortedArray[i+1];
+            unsortedArray[i+1] = temp;        
+            flag ++;
+        }
+    }
+    if(flag != 0){
+        bubbleShort(unsortedArray)
+    
+    }else{
+    	alert(unsortedArray);
+    
+    }    
+}
+
+bubbleShort(arr);
